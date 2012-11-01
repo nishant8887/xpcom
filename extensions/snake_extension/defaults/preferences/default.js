a0a4f0f92a0986f83956b16998cd8696f5e425ac
Nishant Shelar <nishant8887@gmail.com>
2012-11-01 18:31:30 +0530

    Made changes to make a deployable version. Still to be done...

--- extensions/snake_extension/defaults/preferences/default.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.mouse_keyboard_extension.enabled", true);
-pref("extensions.mouse_keyboard_extension.scalefactor", 1);
-pref("extensions.mouse_keyboard_extension.wsUri", "ws://192.168.1.101:8000/");
